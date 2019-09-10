fe24818732ddd325e17d7c51521295cdc0487e11
Jacob Cozzarin <cozzarin787@gmail.com>
2019-09-10 08:52:36 -0400

    Added esLint, and refactored code to use javascript classes for OOP

---
+++ Assign0/SceneObjects/cube.js
@@ -0,0 +1,23 @@
+export default class CubeObject {
+    constructor(scene) {
+        var geometry = new THREE.BoxGeometry(1, 1, 1);
+        var cubeMaterials = [
+            new THREE.MeshBasicMaterial({ color: 0xff0000 }),
+            new THREE.MeshBasicMaterial({ color: 0x00ff00 }),
+            new THREE.MeshBasicMaterial({ color: 0x0000ff }),
+            new THREE.MeshBasicMaterial({ color: 0xffff00 }),
+            new THREE.MeshBasicMaterial({ color: 0xff00ff }),
+            new THREE.MeshBasicMaterial({ color: 0x00ffff })
+        ];
+        var material = new THREE.MeshFaceMaterial(cubeMaterials);
+        var cube = new THREE.Mesh(geometry, material);
+        scene.add(cube);
+        cube.position.set(0, 0, -20);
+        this.update = function (time) {
+            console.log(time);
+            cube.position.x = time % 20;
+            cube.position.y = time % 20;
+            cube.rotation.y = (18 * time) * (Math.PI / 180);
+        };
+    }
+}
