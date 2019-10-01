6dea5bfbda8a7aa7639337e3723a9c36d93455e0
Jacob Cozzarin <cozzarin787@gmail.com>
2019-10-01 11:24:15 -0400

    Implemented key-framing for Assign1

---
+++ Assign1/SceneManager.js
@@ -0,0 +1,89 @@
+import * as THREE from "./three/build/three.module.js";
+import CubeObject from "./SceneObjects/cube.js";
+import KeyFrame from "./KeyFrame.js";
+
+export default class SceneManager {
+    constructor(canvas) {
+        const clock = new THREE.Clock();
+        const screenDimensions = {
+            width: canvas.width,
+            height: canvas.height
+        };
+        const scene = buildScene();
+        const renderer = buildRender(screenDimensions);
+        const camera = buildCamera(screenDimensions);
+        camera.position.set(11, -10, -35);
+        camera.up = new THREE.Vector3(0, 1, 0);
+        camera.lookAt(new THREE.Vector3(11, 0, 0));
+        const sceneObjects = createSceneObjects(scene);
+        const keyFrames = parseKeyFrameFile("keyframe-input.txt");
+        var keyFrameIndex = 0;
+        var oldTime = 0;
+
+        function parseKeyFrameFile(fileName) {
+            var keyFrameList = [];
+            var result = null;
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.open("GET", fileName, false);
+            xmlhttp.send();
+            if (xmlhttp.status==200) {
+                result = xmlhttp.responseText;
+            }
+            var textKeyFrames = result.split("\n");
+            textKeyFrames.forEach(keyFrame => {
+                keyFrameList.push(new KeyFrame(keyFrame));
+            });
+            console.log(keyFrameList)
+            return keyFrameList;
+        }
+        function buildScene() {
+            const scene = new THREE.Scene();
+            scene.background = new THREE.Color("#000");
+            return scene;
+        }
+        function buildRender({ width, height }) {
+            const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true, alpha: true });
+            const DPR = (window.devicePixelRatio) ? window.devicePixelRatio : 1;
+            renderer.setPixelRatio(DPR);
+            renderer.setSize(width, height);
+            renderer.gammaInput = true;
+            renderer.gammaOutput = true;
+            return renderer;
+        }
+        function buildCamera({ width, height }) {
+            const aspectRatio = width / height;
+            const fieldOfView = 60;
+            const nearPlane = 1;
+            const farPlane = 1000;
+            const camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, nearPlane, farPlane);
+            return camera;
+        }
+        function createSceneObjects(scene) {
+            const sceneObjects = [
+                new CubeObject(scene)
+            ];
+            return sceneObjects;
+        }
+        this.update = function () {
+            const elapsedTime = clock.getElapsedTime() - oldTime;
+            if (elapsedTime > keyFrames[keyFrameIndex+1].t) {
+                keyFrameIndex++;
+                if (keyFrameIndex == keyFrames.length - 1) {
+                    keyFrameIndex = 0;
+                    oldTime = clock.getElapsedTime();
+                }
+            }
+            for (let i = 0; i < sceneObjects.length; i++)
+                sceneObjects[i].update(elapsedTime, keyFrames[keyFrameIndex], keyFrames[keyFrameIndex+1]);
+            renderer.render(scene, camera);
+        };
+        this.onWindowResize = function () {
+            const { width, height } = canvas;
+            screenDimensions.width = width;
+            screenDimensions.height = height;
+            camera.aspect = width / height;
+            camera.updateProjectionMatrix();
+            renderer.setSize(width, height);
+        };
+    }
+}
---
+++ Assign1/SceneObjects/cube.js
@@ -0,0 +1,48 @@
+import * as THREE from "../three/build/three.module.js";
+import KeyFrame from "../KeyFrame.js";
+
+export default class CubeObject {
+    constructor(scene) {
+        var geometry = new THREE.BoxGeometry(1, 1, 1);
+        var cubeMaterials = [
+            new THREE.MeshBasicMaterial({ color: 0xff0000 }),
+            new THREE.MeshBasicMaterial({ color: 0x00ff00 }),
+            new THREE.MeshBasicMaterial({ color: 0x0000ff }),
+            new THREE.MeshBasicMaterial({ color: 0xffff00 }),
+            new THREE.MeshBasicMaterial({ color: 0xff00ff }),
+            new THREE.MeshBasicMaterial({ color: 0x00ffff })
+        ];
+        var cube = new THREE.Mesh(geometry, cubeMaterials);
+        scene.add(cube);
+        cube.position.set(0, 0, 0);
+        cube.matrixAutoUpdate = false;
+        // Update method used to animate the object based upon keyframes
+        this.update = function (time, keyFrame1, keyFrame2) {
+            // linear interpolation
+            var u = (time - keyFrame1.t) / (keyFrame2.t - keyFrame1.t);
+
+            // Interpolation based on u value
+            // Translation
+            var p0 = new THREE.Vector3(keyFrame1.pos[0], keyFrame1.pos[1], keyFrame1.pos[2]);
+            var p1 = new THREE.Vector3(keyFrame2.pos[0], keyFrame2.pos[1], keyFrame2.pos[2]);
+            var newPosition = new THREE.Vector3();
+            newPosition.subVectors(p1, p0);
+            newPosition.multiplyScalar(u);
+            newPosition.add(p0);
+            // Orientation
+            var q1 = new THREE.Quaternion();
+            var q2 = new THREE.Quaternion();
+            var axis1 = new THREE.Vector3(keyFrame1.axis[0], keyFrame1.axis[1], keyFrame1.axis[2]);
+            var axis2 = new THREE.Vector3(keyFrame2.axis[0], keyFrame2.axis[1], keyFrame2.axis[2]);
+            q1.setFromAxisAngle(axis1, (Math.PI/180) * keyFrame1.angle).normalize();
+            q2.setFromAxisAngle(axis2, (Math.PI/180) * keyFrame2.angle).normalize();
+            q1.slerp(q2, u);
+            q1.normalize();
+            // Construct Transformation Matrix
+            // Set the rotation matrix of the transformation matrix of the object
+            // Set the position vector of the transformation matrix of the object
+            cube.matrix.compose(newPosition, q1, new THREE.Vector3(2, 2, 2));
+            console.log(cube.matrix);
+        };
+    }
+}
